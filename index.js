1b6c1fa0e3b37112c4b2c5b4a7b7299a39278782
adibro500 <aditya.ammanabrolu@srkaycg.com>
2018-09-04 18:30:12 +0530

    spsoft test

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function setRoutes (app) {
 //         console.log(`[Connected to server on port : ${dbConst.PORT}]`)
 //     }
 
-    app.set('port', (process.env.PORT || 3000))
+    app.set('port', (process.env.PORT || 5000))
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'))
